15b5d053f662b0700e0a16fcf26efa911c002cbe
aleksandra.epifanova <aleksandra.epifanova@elephant-games.am>
2025-12-17 00:28:51 +0400

    [DEV] Add game controller with test grid initialization

---
+++ Assets/Scripts/Core/GameController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private FieldView fieldView;
+
+    private void Start()
+    {
+        var grid = new Grid(5, 5);
+
+        grid.GetCell(0, 0).Block = new Block(BlockType.Blue);
+        grid.GetCell(1, 0).Block = new Block(BlockType.Orange);
+
+        fieldView.Init(grid);
+    }
+}
